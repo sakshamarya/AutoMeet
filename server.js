04c954e568a2b3f5070cf42e17d1ddcc67da7fae
Saksham Arya <sakshamarya01@gmail.com>
2021-11-18 15:52:20 +0530

    Added files

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/',(req,res)=>{
 const ip = process.env.IP || '0.0.0.0'; // For heroku deployment
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
+app.listen(PORT,ip,()=>{
     console.log('server is listening on port 3000');
 });
 
